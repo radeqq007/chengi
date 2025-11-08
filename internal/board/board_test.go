aaf190b1a4ee268e44365e0c6ea1049b0c2a6916
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2025-11-08 15:39:09 +0100

    test: add unit test for generating pawn moves

--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -67,3 +67,143 @@ func TestNew(t *testing.T) {
         }
     }
 }
+
+
+func TestGeneratePawnMoves(t *testing.T) {
+	tests := []struct {
+		name          string
+		setupFunc     func() *Board
+		color         pieces.Color
+		expectedMoves int
+		description   string
+	}{
+		{
+			name: "white pawn single move",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[4][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 1,
+			description:   "Single forward move available",
+		},
+		{
+			name: "white pawn double move from start",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[6][3] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 2,
+			description:   "Single and double move available",
+		},
+		{
+			name: "black pawn double move from start",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[1][5] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.Black,
+			expectedMoves: 2,
+			description:   "Single and double move available",
+		},
+		{
+			name: "white pawn with captures",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[4][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				grid[3][3] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				grid[3][5] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 3,
+			description:   "Forward move + 2 captures",
+		},
+		{
+			name: "black pawn with one capture",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[3][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				grid[4][5] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				return New(grid)
+			},
+			color:         pieces.Black,
+			expectedMoves: 2,
+			description:   "Forward move + 1 capture",
+		},
+		{
+			name: "white pawn blocked",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[4][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				grid[3][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 0,
+			description:   "Blocked by opponent piece",
+		},
+		{
+			name: "white pawn promotion moves",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[1][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 4,
+			description:   "4 promotion options (Q, R, B, N)",
+		},
+		{
+			name: "white pawn promotion with captures",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[1][4] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				grid[0][3] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				grid[0][5] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 12,
+			description:   "4 forward promotions + 4 left capture promotions + 4 right capture promotions",
+		},
+		{
+			name: "black pawn promotion moves",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[6][2] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.Black,
+			expectedMoves: 4,
+			description:   "4 promotion options (Q, R, B, N)",
+		},
+		{
+			name: "white pawn at edge can't double move if blocked",
+			setupFunc: func() *Board {
+				grid := [8][8]pieces.Piece{}
+				grid[6][0] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.White}
+				grid[4][0] = pieces.Piece{Type: pieces.Pawn, Value: 1, Color: pieces.Black}
+				return New(grid)
+			},
+			color:         pieces.White,
+			expectedMoves: 1,
+			description:   "Can only move one square, double blocked",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := tt.setupFunc()
+			moves := b.GenerateMoves(tt.color)
+			if len(moves) != tt.expectedMoves {
+				t.Errorf("%s: expected %d moves, got %d. Description: %s",
+					tt.name, tt.expectedMoves, len(moves), tt.description)
+			}
+		})
+	}
+}
